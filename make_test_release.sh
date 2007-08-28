2ba963a517fb4aca2b89e4107cbf97ef70c045f2
Michael E Brown <michael_e_brown@dell.com>
2007-08-28 15:41:50 -0500

    add script to do autobuild and test releases.

---
+++ make_test_release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# vim:et:ai:ts=4:sw=4:filetype=sh:tw=0:
+
+set -x
+
+cur_dir=$(cd $(dirname $0); pwd)
+cd $cur_dir/
+
+umask 002
+
+# if anything hiccups, halt:
+set -e
+
+chmod -R +w _builddir ||:
+rm -rf _builddir
+
+mkdir _builddir
+pushd _builddir
+../configure
+make -e distcheck
+make -e srpm
+
+. version
+
+/var/ftp/pub/yum/dell-repo/testing/_tools/upload_rpm.sh ./_builddir/dist/SRPMS/${PACKAGE_NAME}-${PACKAGE_VERSION}-*.src.rpm
