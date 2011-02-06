dell/biosdevname

commit 8f686dd1e12e4b4598a1f94f642f4497de108d32

Fix segfault when BIOS advertises zero sized PIRQ Routing Table

src/pirq.c
@@ -63,9 +63,16 @@ struct routing_table * pirq_alloc_read_table()
 			table = (struct routing_table *)(mem+offset);
 			size = table->size;
 			/* quick sanity checks */
+			if (size == 0) {
+				table = NULL;
+				break;
+			}
 			/* Version must be 1.0 */
-			if (! (table->version >> 8)==1 && 
-			      (table->version && 0xFF) == 0) break;
+			if (!((table->version >> 8) == 1 &&
+			      (table->version && 0xFF) == 0)) {
+				table = NULL;
+				break;
+			}
 
 			table = malloc(size);
 			if (!table) break;

commit 638711f773a77fa99351f913a985f28792d9acba

Fix test for PIRQ table version
Bug introduced in 8f686dd1, so in no released version.
Found and fix suggested by Uwe Koziolek.

src/pirq.c
@@ -69,7 +69,7 @@ struct routing_table * pirq_alloc_read_table()
 			}
 			/* Version must be 1.0 */
 			if (!((table->version >> 8) == 1 &&
-			      (table->version && 0xFF) == 0)) {
+			      (table->version & 0xFF) == 0)) {
 				table = NULL;
 				break;
 			}