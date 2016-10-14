dell/biosdevname

commit 51219cac581b5eaced5b172dbbb4586889cb27e1

Don't leak fd on error

src/dmidecode/util.c
@@ -111,6 +111,7 @@ void *mem_chunk(size_t base, size_t len, const char *devmem)
 	if((p=malloc(len))==NULL)
 	{
 		perror("malloc");
+		close(fd);
 		return NULL;
 	}
 	
@@ -129,6 +130,7 @@ void *mem_chunk(size_t base, size_t len, const char *devmem)
 	if(mmp==MAP_FAILED)
 	{
 		free(p);
+		close(fd);
 		return NULL;
 	}
 	
@@ -145,18 +147,21 @@ void *mem_chunk(size_t base, size_t len, const char *devmem)
 		fprintf(stderr, "%s: ", devmem);
 		perror("lseek");
 		free(p);
+		close(fd);
 		return NULL;
 	}
 	
 	if(myread(fd, p, len, devmem)==-1)
 	{
 		free(p);
+		close(fd);
 		return NULL;
 	}
 #endif /* USE_MMAP */
 	
 	if(close(fd)==-1)
 		perror(devmem);
-	
+
+	close(fd);
 	return p;
 }

commit d1d62966032784f292e6875ba4373b18149a5754

Fix use-after-free of fd
This bug got introduced in 51219cac581b5eaced5b172dbbb4586889cb27e1. I
overlooked that we are already properly closing fd on non-error code
path and I added redundant call to close.

Closing already closed fd is harmless though. Function returns EBADF and
fails silently.

src/dmidecode/util.c
@@ -162,6 +162,5 @@ void *mem_chunk(size_t base, size_t len, const char *devmem)
 	if(close(fd)==-1)
 		perror(devmem);
 
-	close(fd);
 	return p;
 }